71263723232cb98ea213f0cbbcfb721c1e718141
Meaghan Jones <meaghan.m.jones@gmail.com>
2016-07-14 08:02:43 -0700

    fix integration specs

--- app.rb
+++ app.rb
@@ -9,11 +9,6 @@ get('/') do
   erb(:index)
 end
 
-# get('/words') do
-#   @words = Word.all()
-#   erb(:words)
-# end
-
 get('/words/new') do
   erb(:words_form)
 end
--- spec/word_definition_integration_spec.rb
+++ spec/word_definition_integration_spec.rb
@@ -3,6 +3,11 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
+describe(Definition) do
+  before() do
+    Definition.clear()
+  end
+
 describe('the add word path', {:type => :feature})do
   it('navigate to the word form') do
     visit('/')
@@ -20,10 +25,13 @@ end
 
 describe('the add definition path', {:type => :feature}) do
   it('processes the user definition entry and confirms it was successfully added') do
+    word = Word.new({:name => 'ginourmous', :definitions => nil, :id => nil})
+    word.save()
     visit('/words/:id/definitions/new')
     fill_in('description', :with => 'really big')
     fill_in('partofspeech', :with => 'adjective')
     click_button('Add definition')
     expect(page).to have_content('Success!')
   end
+end
 end
